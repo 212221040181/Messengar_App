01319801e8b0e509b43d44970ef1021fedfc1cfa
Vishnu priya <vishnupriya7299549305@gmail.com>
2023-08-13 19:54:54 +0530

    Messanger app

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        SMSMessagingService smsMessagingService = new SMSMessagingService();
+        EmailMessagingService emailMessagingService = new EmailMessagingService();
+        WhatsAppMessagingService whatsAppMessagingService = new WhatsAppMessagingService();
+        smsMessagingService.sendMessage();
+        MessagingService.message();
+        emailMessagingService.sendMessage();
+        whatsAppMessagingService.sendMessage();
+        MessagingService.message();
+
+    }
+}
+interface MessagingService {
+    static void message()
+    {
+        System.out.println("Hello");
+    }
+    abstract  void sendMessage();
+}
+class SMSMessagingService implements MessagingService
+{
+    @Override
+    public void sendMessage() {
+        System.out.println("hi im sms");
+    }
+}
+class EmailMessagingService implements MessagingService
+{
+    @Override
+    public void sendMessage() {
+        System.out.println("im email");
+    }
+}
+class WhatsAppMessagingService implements MessagingService
+{
+    @Override
+    public void sendMessage() {
+        System.out.println("im whatsapp");
+    }
+}
